fef0a1aaabdeb449f7189eecc88df67c49ae7585
Uaurá Menezes <uaurasilva@hotmail.com>
2021-03-01 18:56:54 -0300

    Add search for values not found on vec

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 struct P {
-    l: usize,
-    r: usize,
+    l: i32,
+    r: i32,
 }
 
 fn main() {
-    let x = 33;
+    let x = 4;
 
-    let mut vec = Vec::new();
-    for n in 1..=100 {
-        vec.push(n);
-    }
+    //let vec = vec!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    let vec = vec!(1, 3, 5, 7, 9);
+    //let vec = vec!(2, 4, 6, 8, 10);
 
-    let pointer = P{l: 0, r: vec.len() - 1};
-    let index = bts(&vec, x, pointer);
+    let pointer = P{l: 0, r: vec.len() as i32};
 
-    println!("The index of {} is {}", x, index);
+    match search(&vec, x, pointer) {
+        -1 => println!("Element Not Found!"),
+        i => println!("The index of {} is {}", x, i)
+    };
 }
 
-fn bts(vec: &Vec<i32>, x: i32, mut p: P) -> usize {
-    let i = (((p.l + p.r) as i32 / 2).abs()) as usize;
+fn search(vec: &Vec<i32>, x: i32, mut p: P) -> i32 {
+    let middle = ((p.l + p.r) / 2).abs();
+    let i = middle as usize;
 
-    if vec[i] == x {
-        i
+    if middle < 0 || i >= vec.len() {
+        -1
+    } else if vec[i] == x {
+        middle
+    } else if p.l >= p.r {
+        - 1
     } else if vec[i] > x {
-        p.r = i - 1;
-        bts(vec, x, p)
+        p.r = middle - 1;
+        search(vec, x, p)
     } else{
-        p.l = i + 1;
-       bts(vec, x, p)
+        p.l = middle + 1;
+        search(vec, x, p)
     }
 }
