d1f219b2baa6115ef3974b798d30a080fe566eda
uauramenezes <uaurasilva@hotmail.com>
2021-05-13 17:21:59 -0300

    Add Bubble Sort

---
+++ sort/bubble_sort.py
@@ -0,0 +1,5 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
